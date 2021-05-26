0e6d8b5634bae561182227d723ce481579a1f70c
DaniPoot <toniuc.6@gmail.com>
2021-05-26 00:57:55 -0500

    Implementing the SearchMoviesService to search a movie

--- src/app/modules/search/movies-found/movies-found.component.ts
+++ src/app/modules/search/movies-found/movies-found.component.ts
@@ -1,5 +1,7 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { Genre } from 'src/app/core/classes/genre/genre';
+import { SearchMoviesService } from 'src/app/core/services/shearchMovies/search-movies.service';
 import { Movie } from '../../../core/classes/movie/movie';
 import { FindMovieService } from '../../../core/services/findMovie/find-movie.service';
 import { GenresService } from '../../../core/services/genres/genres.service';
@@ -14,7 +16,7 @@ export class MoviesFoundComponent implements OnInit {
   movies: Array<Movie> = [ ];
   query: string="";
   genres: Array<Genre> = [ ];
-  constructor(private findMovieService: FindMovieService, private genresService: GenresService ) {
+  constructor(private findMovieService: FindMovieService, private genresService: GenresService, private search: SearchMoviesService) {
 
   }
 
@@ -25,12 +27,14 @@ export class MoviesFoundComponent implements OnInit {
       }, error => console.error(error)
       
     )
+
+    this.search.query$.subscribe( (value) => this.onKeyupEvent(value))
   
   }
 
-  onKeyupEvent(e: Event){
-    if(this.query.length >= 3){
-      this.findMovieService.findMovie(this.query).then(
+  onKeyupEvent(value: string){
+    if(value.length >= 3){
+      this.findMovieService.findMovie(value).then(
         response => { this.movies = response.results as Movie[]
         }, error => console.error(error)
       )
