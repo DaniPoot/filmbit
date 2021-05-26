b14499f4cf75343b4a0bd4fbc38a13c36d7fff86
DaniPoot <toniuc.6@gmail.com>
2021-05-26 01:01:27 -0500

    Implementing the SearchMoviesService to display MoviesFoundComponent

--- src/app/modules/home/layout/layout.component.ts
+++ src/app/modules/home/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchMoviesService } from 'src/app/core/services/shearchMovies/search-movies.service';
 
 @Component({
   selector: 'app-layout',
@@ -6,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
+  public userIsSearch: boolean = false
 
-  constructor() {
-    
-  }
+  constructor(private search: SearchMoviesService) { }
 
   ngOnInit(): void {
-    
+    this.search.query$.subscribe((value) => {
+      this.userIsSearch = value.length > 0  
+    })
   }
 
 }
