cd22560223d1ef407d53f271c739d3c7e2b596e7
DaniPoot <danielpoot.uc2@gmail.com>
2021-06-26 17:02:15 -0500

    Implemented function to protect routes

--- src/app/core/services/login/login.service.ts
+++ src/app/core/services/login/login.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { CanActivate } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
+export class LoginService implements CanActivate {
 
   isLogged$ = new EventEmitter<Boolean>()
 
   constructor() { }
+
+  canActivate(): boolean {
+    return localStorage.getItem('isLogged') === "true";
+  }
 }
