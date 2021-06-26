9c588768771a8f9010d88a4fae77e7f0514719ca
DaniPoot <danielpoot.uc2@gmail.com>
2021-06-26 17:11:00 -0500

    Added new route for user's favorite movies and protect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginService } from './core/services/login/login.service';
 import { LayoutComponent as DetailsComponent } from './modules/details/layout/layout.component';
 import { LayoutComponent as HomeComponent } from './modules/home/layout/layout.component';
 import { LayoutComponent as LoginComponent } from './modules/login/layout/layout.component';
 import { LayoutComponent as RegisterComponent } from './modules/register/layout/layout.component';
+import { LayoutComponent as FavoritesComponent } from './modules/favorites/layout/layout.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'details/:id', component: DetailsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [LoginService] },
   { path: '**', redirectTo: '/' }
 ];
 
